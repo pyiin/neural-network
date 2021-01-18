4a6c22ddcf48dc6006d45fb21c779592532b41af
Piotr Maksymiuk <2003piotr@gmail.com>
2021-01-18 18:45:52 +0100

    Edited .gitignore

---
+++ src/neural-network.cpp
@@ -0,0 +1,19 @@
+#include "neural-network.h"
+
+class NeuralNetwork {
+    
+    private:
+    struct node {
+        double value;
+        double baias;
+    };
+    int* layers_size;
+    node* nodes;
+    double* edges_weight;
+
+    public:
+    void generateNew();
+    double getNodeValue(int, int);
+    void setNodeBaias(int, int, double);
+    void setEdgeWeight(int, int, int, double);
+};
