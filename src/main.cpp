5c9b2bc0c1c8b41b3e0a95ffcb8a27b0cf10b4af
Piotr Maksymiuk <2003piotr@gmail.com>
2021-01-19 20:11:06 +0100

    New main.cpp to debug

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,12 @@ using std::cout;
 using std::endl;
 
 int main() {
-	cout << "Hello World!" << endl;
-
-		return 0;
+	NeuralNetwork NNN;
+	int layers[3] = {3, 5, 3};
+	double input[3] = {3, 5, 3};
+	double output[3] = {3, 5, 3};
+	NNN.generateNew(3, layers);
+	cout << NNN.evaluate(input, output) << endl;
+	
+	return 0;
 }
