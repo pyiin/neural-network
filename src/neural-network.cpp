cba11ab5b7c64ac0be4291bb6bd49b4ae2553c9d
pyiin <pyiin.ab@gmail.com>
2021-01-19 08:51:32 +0100

    tabs fix

--- src/neural-network.cpp
+++ src/neural-network.cpp
@@ -16,37 +16,39 @@
 
     public:
     */
-    void NeuralNetwork::generateNew(int new_layers_num, int* new_layers_sizes) {
-        layers_num = new_layers_num;
-        layers_sizes = new int[layers_num];
-        for(int layer = 0; layer < layers_num; layer++) {
-            layers_sizes[layer] = new_layers_sizes[layer];
-            nodes_num += layers_sizes[layer];
-            if(layer > 0) {
-                edges_num += layers_sizes[layer] * layers_sizes[layer - 1];
-            }
+void NeuralNetwork::generateNew(int new_layers_num, int* new_layers_sizes) {
+    layers_num = new_layers_num;
+    layers_sizes = new int[layers_num];
+    for(int layer = 0; layer < layers_num; layer++) {
+        layers_sizes[layer] = new_layers_sizes[layer];
+        nodes_num += layers_sizes[layer];
+        if(layer > 0) {
+            edges_num += layers_sizes[layer] * layers_sizes[layer - 1];
         }
-        nodes = new node[nodes_num];
-        edges_weight = new double[edges_num];
     }
-    double NeuralNetwork::evaluate(double* input) {
-		int k = layers_sizes[0];
-        for(i = 1; i < layers_num; i++){
-			for(int j=0; j < layers_sizes[i]; j++){
-				for(int l=0; l < layers_sizes[i-1]; l++){
-					nodes[k+j].value += edges_weight[l*layers_sizes[i-1] + i] * nodes[k - layers_sizes[i-1] + l].value;
-				}
-				nodes[k+j].value += nodes[k+j].bias;
-				nodes[k+j].value = sigmoid(nodes[k+j].value);
-			}
-	    }
-    }
-    double NeuralNetwork::getNodeValue(int layer_n, int layer_k) {
+    nodes = new node[nodes_num];
+    edges_weight = new double[edges_num];
+}
 
+double NeuralNetwork::evaluate(double* input) {
+    int k = layers_sizes[0];
+    for(i = 1; i < layers_num; i++){
+        for(int j=0; j < layers_sizes[i]; j++){
+            for(int l=0; l < layers_sizes[i-1]; l++){
+                nodes[k+j].value += edges_weight[l*layers_sizes[i-1] + i] * nodes[k - layers_sizes[i-1] + l].value;
+            }
+            nodes[k+j].value += nodes[k+j].bias;
+            nodes[k+j].value = sigmoid(nodes[k+j].value);
+        }
     }
-    void NeuralNetwork::setNodeBaias(int, int, double) {
+}
 
-    }
-    void NeuralNetwork::setEdgeWeight(int, int, int, double) {
+double NeuralNetwork::getNodeValue(int layer_n, int layer_k) {
 
-    }
+}
+void NeuralNetwork::setNodeBaias(int, int, double) {
+
+}
+void NeuralNetwork::setEdgeWeight(int, int, int, double) {
+
+}
